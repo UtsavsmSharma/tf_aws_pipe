86da3ace2dd6a88345c721c2331f9f33342d00f9
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-04-05 10:02:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 # S3 Bucket resource
-resource "aws_s3_bucket" "example_bucket" {
+resource "aws_s3_bucket" "my_example_bucket" {
   bucket = "demotxchd"
   acl    = "private"
 }
